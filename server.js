65d238c0a224f1ed58a3c2bd3c6d44d7843c9306
José González <a01038061@itesm.mx>
2020-09-21 15:26:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,17 +6,14 @@ app.use(cors())
 app.use(express.json())
 const port = 8080;
 
-app.get('/:pokemonName', (req, res, next) => {
+//Backend function to connect to the api
+app.get('/:pokemonName', (req, res) => {
   let pokemon_name = req.params.pokemonName;
   axios
     .get(`http://pokeapi.co/api/v2/pokemon/${pokemon_name}`) 
     .then(pokemon_response => {
-        //Save data in dictionary
-        //pokemons[pokemon_name] = pokemon_response.data;
         //console.log(pokemon_response.data); 
-        //return dictionary with pokemons
-        res.send(pokemon_response.data);
-        //Aquí está la data del pokemon
+        res.send(pokemon_response.data); //Aquí está la data del pokemon
     }).catch(function(error) {
         console.log(error)
     });
